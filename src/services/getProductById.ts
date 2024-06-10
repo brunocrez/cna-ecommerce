64c3dd5f74f53b3a5e4ec6b32e5d5a493b10cf88
Bruno Rezende <brunoc.rez@gmail.com>
2024-06-10 09:52:36 -0300

    refactor: url at getProductById

--- src/services/getProductById.ts
+++ src/services/getProductById.ts
@@ -3,7 +3,7 @@ import { IProduct } from '@/interfaces/Product'
 import { api } from './api/api-routes'
 
 export async function getProductById(id: string) {
-  const response: AxiosResponse<IProduct> = await api.get(`products/${id}`)
+  const response: AxiosResponse<IProduct> = await api.get(`/products/${id}`)
 
   return response.data
 }
